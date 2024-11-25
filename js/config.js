813e07fa2bb09edb793c8ca5d1777522883ea730
linkuello <144430394+linkuello@users.noreply.github.com>
2024-11-25 10:45:22 +0600

    Update config.js erlan kot

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 window._config = {
   cognito: {
-    userPoolId: "us-west-2_Gl1gpDfZt",
-    userPoolClientId: "48dnc3fu1rjsg8lifvpms7cslm",
-    region: "us-west-2"
+    userPoolId: "us-east-2_9CYlQpbjT",
+    userPoolClientId: "fq0i6vr69hp4nv10861ngo8vu",
+    userPoolClientSecret: "1pjnh197oqpsq03n7ul03u08q4nh23a7ltejphj77n34tm4lnjdt",  // Your provided App Client Secret
+    region: "us-east-2"
   },
   api: {
     invokeUrl: ""

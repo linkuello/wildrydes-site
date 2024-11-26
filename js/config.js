6011e01f19330133f3431dfca61f66e8535b7c36
linkuello <144430394+linkuello@users.noreply.github.com>
2024-11-26 14:36:51 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
   cognito: {
-    userPoolId: "us-east-2_OMSUAd6QF",
-    userPoolClientId: "13i2gcc71grich6dr3a7pt52q2",
+    userPoolId: "us-east-2_48Bzg9eK5",
+    userPoolClientId: "4uk2depkaqli4l5c8g4hctarsq",
     userPoolClientSecret: "7jht9aqejtmngel0pklsuna4rcrt9aefok79frit2a2gcsordvm",  // Your provided App Client Secret
     region: "us-east-2"
   },
   api: {
-    invokeUrl: ""
+    invokeUrl: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_48Bzg9eK5/.well-known/jwks.json"
   }
 };
